8b75da93078b7035d34b948b6621c0c80776fdfd
Tafadzwa Muteke <muteket@gmail.com>
2019-11-06 12:10:57 +0200

    Intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#081c24",
+        accent: "#01d277"
+      },
+      fontFamily: {
+        "circularstd-bold": ["circularstd-bold", "helvetica", "sans-serif"],
+        "circularstd-bolditalic": [
+          "circularstd-bolditalic",
+          "helvetica",
+          "sans-serif"
+        ],
+        "circularstd-book": ["circularstd-book", "helvetica", "sans-serif"],
+        "circularstd-bookitalic": [
+          "circularstd-bookitalic",
+          "helvetica",
+          "sans-serif"
+        ],
+        "circularstd-black": ["circularstd-black", "helvetica", "sans-serif"],
+        "circularstd-blackitalic": [
+          "circularstd-blackitalic",
+          "helvetica",
+          "sans-serif"
+        ],
+        "circularstd-medium": ["circularstd-medium", "helvetica", "sans-serif"],
+        "circularstd-mediumitalic": [
+          "circularstd-mediumitalic",
+          "helvetica",
+          "sans-serif"
+        ]
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
